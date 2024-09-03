e9c1fcdc1d1e4e0cd6e75c4ce43d01d706cb656a
Samson Ocran <samson.ocran@gmail.com>
2024-09-03 02:18:41 +0100

    error handling with constant file

---
+++ routes/middleware/errorHandler.js
@@ -0,0 +1,40 @@
+const {constants} = require('../../constants ')
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({ title: "Validation fail", message: err.message });
+      break;
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      case constants.UNAUTHORIZED:
+      res.json({
+        title: "Not Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      case constants.FORBIDEEN:
+      res.json({
+        title: "Not Forbideen",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      case constants.SERVER_ERROR:
+      res.json({
+        title: "Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+
+    default:
+      break;
+  }
+};
+
+module.exports = errorHandler;
